b03be84d0757837f7da9f4cab1620f14430388ad
hiromasa_tabuchi <hiromasa.t@gmail.com>
2019-06-26 06:48:38 +0000

    fix gitignore

--- Bot/src/repeatYourWord/.debug/repeatYourWord/lambda_function.py
+++ Bot/src/repeatYourWord/.debug/repeatYourWord/lambda_function.py
@@ -11,8 +11,14 @@ from lib.linebot.exceptions import (
 )
 import logging
 
+# log取得用
+
 logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
+
+# reference
+# https://github.com/line/line-bot-sdk-python
+# https://developers.line.biz/ja/docs/messaging-api/overview/
 
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
@@ -28,9 +34,18 @@ line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 def lambda_handler(event, context):
-    reply_token = event["events"][0]["replyToken"]
+    logger.info(event) # lambdaが受けた取ったjsonの確認。cloudwatch logで見れる
+    
+    # reply用のtoken。これで誰のメッセージにレスするかが決まる。
+    reply_token = event["events"][0]["replyToken"] 
+    # 送信されたtext本文
     message = event["events"][0]["message"]["text"]
 
+    ###
+    # ここに実装
+    ###
+
+    # line botへのメッセージの返信。local test中はreply_tokenの更新が大変なのでreturnするなりしてテストしたほうが良いです。
     line_bot_api.reply_message(reply_token, TextSendMessage(text=message))
     
     return "reply complete"
