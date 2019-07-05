aba610c10ea245e5afeb99e26d4a78e54894d9ae
kyohei <software.engineering.unics@gmail.com>
2019-07-05 23:23:51 +0900

    bettoteの待ち受け処理を実装

--- RPi/src/monitor.py
+++ RPi/src/monitor.py
@@ -3,58 +3,66 @@
 import paho.mqtt.client as mqtt
 import json
 import requests
-import numpy as np
-import cv2
-import ipget
-from datetime import datetime
+from take_photo_picam import take_photo
+import os
+import subprocess
+import traceback
+import logging
 
-TOKEN = ""
-TOPIC = ""
+logger = logging.getLogger(__name__)
+fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
+logging.basicConfig(level=logging.DEBUG, format=fmt)
+TOKEN = None
+with open('bebotte_token','r') as f:
+    TOKEN = f.readlines()[0]
+
+TOPIC = "IoT_Refrigerator/take_req"
 HOSTNAME = "mqtt.beebotte.com"
 PORT = 8883
+CACERT = "mqtt.beebotte.com.pem"
+PROFILE_NAME = "sse2019-group1"
+SEND_COMMAND_TEMPLATE = "aws s3 cp {0} {1} --profile={2}"
+DEBUG = True
 
-CACERT = "mqtt.beebottle.com.pem"
-dir_path = "./img/"
-ip = ipget.ipget()
-ServerIP = "{}:5000".format(ip.ipaddr("wlan0").split("/")[0])
+def send_img_to_s3(file_name, bucket_name):
+    send_command = SEND_COMMAND_TEMPLATE.format(file_name, bucket_name, PROFILE_NAME)
+    if DEBUG:
+        logger.info(send_command)
+    else:
+        logger.info(send_command)
+        subprocess.call(send_command, shell=True)
+    return 
 
 def on_connect(client, userdata, flags, respons_code):
     print('status {0}'.format(respons_code))
+    logger.info('status {0}'.format(respons_code))
     client.subscribe(TOPIC)
-
-def send_item_change_request(command, item, amount):
-    url = "http://{0}/{1}?item={2}&amount={3}".format(ServerIP, command, item, amount)
-    print(url)
-    requests.get(url)
-    return 
-
-def take_and_send_photo():
-    cap = cv2.VideoCapture(0)
-    file_name = "{0}{1}.jpeg".format(dir_path, datetime.now().strftime("%Y%m%d%H%M%S"))
-    ret, frame = cap.read()
-    cv2.imwrite(file_name, frame)
-    print("send image :{}".format(file_name))
-    cap.release()
-
+    logger.info("start to subscribe: " + TOPIC)
 
 def on_message(client, userdata, msg):
-    data = json.loads(msg.payload.decode("utf-8"))["data"][0]
-    data = {key:value.strip() for key, value in data.items()}
-    action = data['action']
-    print(data)
-    if action == u"add":
-        item = data['item']
-        amount = data['num']
-        send_item_change_request("Add", item, amount)
-    elif action == u"reduce":
-        item = data['item']
-        amount = data['num']
-        send_item_change_request("Reduce", item, amount)
-    elif action == u"take":
-        take_and_send_photo()
-    else:
-        print(data)
-        
+    try:
+        data = json.loads(msg.payload.decode("utf-8"))["data"]
+        if type(data) is str:
+            # データが文字列で渡されてるので、そこだけ再度、オブジェクト化
+            data = json.loads(data)
+        else:
+            data = data
+
+        data = {key:value.strip() for key, value in data.items()}
+        action = data['req_type']
+        bucket_name = data['s3_bucket']
+        logging.info("aciton='{0}', bucket_name='{1}'".format(action, bucket_name))
+        if action == "take_photo":
+            file_name = "tmp.jpeg"
+            take_photo(file_name)
+            if os.path.exists(file_name):
+                send_img_to_s3(file_name, bucket_name)
+            else:
+                logging.error("failed to take photo")
+        else:
+            logging.warn(data)
+    except:
+        traceback.print_exc()
 client = mqtt.Client()
 client.username_pw_set("token:%s"%TOKEN)
 client.on_connect = on_connect
--- RPi/src/take_photo_picam.py
+++ RPi/src/take_photo_picam.py
@@ -4,19 +4,19 @@ import RPi.GPIO as GPIO
 import picamera
 from datetime import datetime
 
-dir_path = "./img/"
-
 def take_photo(file_name):
     try:
-    
         #Take Picture
         camera = picamera.PiCamera()
         camera.capture(file_name)
-
     except KeyboardInterrupt:
         pass
+    finally:
+        camera.close()
+
+
     
 
 if __name__ == "__main__":
-    file_name = "{0}{1}.jpeg".format(dir_path, datetime.now().strftime("%Y%m%d%H%M%S"))
+    file_name = "{0}.jpeg".format(datetime.now().strftime("%Y%m%d%H%M%S"))
     take_photo(file_name)
